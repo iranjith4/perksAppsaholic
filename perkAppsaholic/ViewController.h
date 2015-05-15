9cd7c1c490b2701fc666f56db7e95fef5f2ee111
Ranjithkumar Matheswaran <iranjith4@gmail.com>
2015-05-15 21:13:58 +0530

    Initial Commit

---
+++ perkAppsaholic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  perkAppsaholic
+//
+//  Created by Ranjith on 15/05/15.
+//  Copyright (c) 2015 RanjithKumar Matheswaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ perkAppsaholic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  perkAppsaholic
+//
+//  Created by Ranjith on 15/05/15.
+//  Copyright (c) 2015 RanjithKumar Matheswaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
